c53c5fe80fe01f35a096c616c148c50dd765c765
Chris Tomich <chris.tomich@gmail.com>
2018-10-14 18:27:54 +1100

    Have a basic calculator parser completed

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/chris-tomich/console-calculator/console"
 )
@@ -12,7 +13,7 @@ func main() {
 	var input string
 	var err error
 
-	for input != "exit" && input != "quit" {
+	for CalculatorFunctions(input) {
 		input, err = c.Prompt()
 
 		if err != nil {
@@ -20,7 +21,64 @@ func main() {
 
 			return
 		}
+	}
+}
 
-		c.Write(input)
+// CalculatorFunctions will perform the operation that has been given as user input.
+func CalculatorFunctions(input string) bool {
+	switch strings.ToLower(input) {
+	case "exit":
+		return false
+	case "quit":
+		return false
+	}
+
+	seperatedFields := FieldsFuncWithSeparator(input, SeparatorTester("/*-+"), SeparatorTester(" "))
+
+	for _, field := range seperatedFields {
+		fmt.Println("'" + field + "'")
+	}
+
+	return true
+}
+
+// FieldsFuncWithSeparator will separate a string given the separators but include the separators.
+func FieldsFuncWithSeparator(s string, separatorsToInclude func(rune) bool, separatorsToExclude func(rune) bool) []string {
+	spans := make([]string, 0, 32)
+
+	start := 0
+
+	for i, r := range s {
+		if separatorsToInclude(r) {
+			if start != i {
+				spans = append(spans, s[start:i])
+			}
+
+			spans = append(spans, s[i:i+1])
+			start = i + 1
+		} else if separatorsToExclude(r) {
+			if start != i {
+				spans = append(spans, s[start:i])
+			}
+
+			start = i + 1
+		}
+	}
+
+	spans = append(spans, s[start:])
+
+	return spans
+}
+
+// SeparatorTester will return a tester for a string of separators.
+func SeparatorTester(seps string) func(rune) bool {
+	return func(r rune) bool {
+		for _, sep := range seps {
+			if r == sep {
+				return true
+			}
+		}
+
+		return false
 	}
 }
